4b337824b7bf489e0b493ee0ab7f00c7ee6f80ed
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-10-18 07:56:30 -0400

    Add files via upload
    nav links mobile menu improved.  slide.js complete

--- slide.js
+++ slide.js
@@ -1,19 +1,61 @@
-const carouselSlide = document.querySelector(".carousel__slide")
-const carouselImage = document.querySelectorAll(".carousel__slide img")
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
 
-// buttons
-const prevButton = document.querySelector(".prev__button")
-const nextButton = document.querySelector(".next__button")
+const nextSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for next slide
+    if (current.nextElementSibling) {
+        // Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        // Add current to start
+        slides[0].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
 
-//counter 
-let counter = 1;
+const prevSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check for prev slide
+    if (current.previousElementSibling) {
+        // Add current to prev sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        // Add current to last
+        slides[slides.length - 1].classList.add('current');
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
 
-const size = carouselImage[0].clientWidth;
-carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"
+// Button events
+next.addEventListener('click', e => {
+    nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
 
-// button events 
-nextButton.addEventListener("click", () => {
-    carouselSlide.style.transition = "transform 0.4s ease-in-out"
-    counter++;
-    console.log(counter)
-})
+prev.addEventListener('click', e => {
+    prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+});
+
+// Auto slide
+if (auto) {
+    // Run next slide at interval time
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
